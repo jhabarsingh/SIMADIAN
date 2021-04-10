0b76100eae46fb92547d9fc87cc6565dfcecb59d
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-04-10 14:34:44 +0530

    created user app

---
+++ simadian/commerce/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+app_name = 'commerce'
+
+urlpatterns = [
+    # path('', UsersListApiView.as_view(), name='list'),
+]
---
+++ simadian/users/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+from .models import MyUser, Profile
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MyUser
+        fields = (
+            'username',
+            'first_name',
+            'last_name',
+            'date_of_birth',
+            'email',
+            'password'
+        )
+
+    def create(self, validated_data):
+        '''
+        Method is called when serializers.save()
+        '''
+        user = MyUser.objects.create_user(
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            date_of_birth=validated_data['date_of_birth'], 
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        
+        return user
+        
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = (
+            'profile_pics'
+        )
