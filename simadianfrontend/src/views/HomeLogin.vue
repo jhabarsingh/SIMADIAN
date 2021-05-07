2f9e69773208d149e990649f0d20e5fb85601930
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-05-08 01:50:22 +0530

    Added Application component

--- simadianfrontend/src/views/HomeLogin.vue
+++ simadianfrontend/src/views/HomeLogin.vue
@@ -4,17 +4,32 @@
       v-model="drawer"
       app
     >
-      <!--  -->
+        <v-btn
+        :loading="loading3"
+        :disabled="loading3"
+        color="blue-grey"
+        class="ma-2 white--text"
+        @click="loader = 'loading3'"
+        >
+        Upload
+        <v-icon
+            right
+            dark
+        >
+            mdi-cloud-upload
+        </v-icon>
+        </v-btn>
+
     </v-navigation-drawer>
 
     <v-app-bar app>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>SIMADIAN</v-toolbar-title>
     </v-app-bar>
 
     <v-main>
-      <!--  -->
+
     </v-main>
   </v-app>
 </template>
