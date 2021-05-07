f269daba7c09442c2e59e4f552042e4a0f90fa1b
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-05-08 01:42:09 +0530

    Added Home page component

--- simadianfrontend/src/views/HomeLogin.vue
+++ simadianfrontend/src/views/HomeLogin.vue
@@ -1,14 +1,26 @@
 <template>
-    <home-login />
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <!--  -->
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HomeLogin from '../components/HomeLogin';
-
-export default {
-    components: {
-        HomeLogin,
-    }
-}
-
+  export default {
+    data: () => ({ drawer: null }),
+  }
 </script>
