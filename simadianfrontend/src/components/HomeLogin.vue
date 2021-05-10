f6e45b44bc6492ae29a84cd9a13475aeb85b6053
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-05-11 02:11:23 +0530

    Added Login, Register buttonxa

--- simadianfrontend/src/components/HomeLogin.vue
+++ simadianfrontend/src/components/HomeLogin.vue
@@ -4,7 +4,49 @@
     <v-app-bar app>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>SIMADIAN</v-toolbar-title>
+      <v-toolbar-title>
+        <v-tab>
+          <v-badge
+            color="pink"
+            dot
+          >
+            SIMADIAN
+          </v-badge>
+        </v-tab>
+      </v-toolbar-title>
+
+      <v-divider style="margin: 0px 10px;" />
+
+      <v-btn
+        class="ma-2"
+        color="primary"
+        dark
+        rounded
+      >
+        Accept
+        <v-icon
+          dark
+          right
+        >
+          mdi-login
+        </v-icon>
+      </v-btn>
+
+      <v-btn
+        class="ma-2"
+        color="warning"
+        dark
+        rounded
+      >
+        Accept
+        <v-icon
+          dark
+          right
+        >
+          mdi-account
+        </v-icon>
+      </v-btn>
+
     </v-app-bar>
 
     <v-main>
