4c7def9794b6f010c3c24c510eebfc229a27be8f
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-05-11 09:06:25 +0530

    Added Logout Card component

---
+++ simadianfrontend/src/components/Logout.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-parallax
+    dark
+    src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+    style="height:100vh;"
+  >
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col
+        class="text-center"
+        cols="12"
+      >
+        <LogoutCard />
+      </v-col>
+    </v-row>
+  </v-parallax>
+</template>
+
+<script>
+import LogoutCard from './LogoutCard.vue';
+
+export default {
+    components: {
+        LogoutCard,
+    }
+}
+
+</script>
---
+++ simadianfrontend/src/components/LogoutCard.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-card
+    class="mx-auto"
+    max-width="344"
+  >
+    <v-img
+      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+      height="200px"
+    ></v-img>
+
+    <v-card-title
+        style="display:flex;justify-content:center;"
+    >
+      SIMADIAN
+    </v-card-title>
+        👋
+    <v-card-subtitle>
+      
+    </v-card-subtitle>
+
+    <v-card-actions>
+      <v-btn
+        color="orange lighten-2"
+        text
+        @click="$router.push('/home')"
+      >
+        Logout
+      </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        icon
+        @click="show = !show"
+      >
+        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+      </v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider></v-divider>
+
+        <v-card-text>
+          Gratitude is the healthiest of all human emotions. The more you express gratitude for what you have, the more likely you will have even more to express gratitude for.
+        </v-card-text>
+      </div>
+    </v-expand-transition>
+  </v-card>
+</template>
+
+
+<script>
+  export default {
+    data: () => ({
+      show: false,
+    }),
+  }
+</script>
