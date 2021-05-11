28d8173e0bdbd0d5563af3d05f5b21cc6ac92e0c
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-05-11 20:26:30 +0530

    Added Message component

---
+++ simadianfrontend/src/components/items/WriteMessageCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-parallax
+    dark
+    src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
+    style="min-height:100vh;"
+  >
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col class="text-center" cols="12">
+        <write-message />
+      </v-col>
+    </v-row>
+  </v-parallax>
+</template>
+
+<script>
+import WriteMessage from './WriteMessage.vue'
+
+export default {
+  data: () => ({
+
+  }),
+  components: {
+    WriteMessage
+  }
+}
+
+</script>
