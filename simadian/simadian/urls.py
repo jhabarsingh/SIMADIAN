ef726baaca4ccfcdcb0661bf5629ecdea3b8bc41
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-05-14 17:59:48 +0530

    Added Verify token Route

--- simadian/simadian/urls.py
+++ simadian/simadian/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('items/', include('commerce.urls')),
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', jwt_views.TokenVerifyView.as_view(), name='token_verify'),
 ]
