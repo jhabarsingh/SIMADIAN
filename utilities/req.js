d470041e50ff5dab3bbd3c0ab609e583b89dd350
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-05-24 17:22:37 +0530

    Added res.js for register

---
+++ utilities/req.js
@@ -0,0 +1,24 @@
+URL = "http://ec2-18-224-181-83.us-east-2.compute.amazonaws.com/users/create/"
+
+user = {
+      "username": "jhabarsinghbhat12i",
+      "first_name": "jhabar",
+      "last_name": "singh",
+      "date_of_birth": "2021-05-08",
+      "email": "jhabar@msrit.edu",
+      "password": "Jhabar@123"
+}
+
+let options = {
+	method: 'POST',
+        headers: {
+		'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+ }
+
+fetch(URL, options).then(res => {
+	return res.json();
+}).then(res => {
+	console.log(res);
+});
