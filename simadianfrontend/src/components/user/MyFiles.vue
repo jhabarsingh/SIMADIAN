10c1c1b3be22f0b8e25d1f1bdf1903aacb74cebf
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-05-27 08:23:43 +0530

    Refactored MYFile Component

--- simadianfrontend/src/components/user/MyFiles.vue
+++ simadianfrontend/src/components/user/MyFiles.vue
@@ -22,36 +22,13 @@
         multiple
       >
         <template v-for="(item, index) in items">
-          <v-list-item :key="item.title" @click="$router.push('/message-detail')">
+          <v-list-item :key="item.title" @click="goTo">
             <template v-slot:default="{ active }">
               <v-list-item-content>
-                <v-list-item-title v-text="item.title"></v-list-item-title>
 
-                <v-list-item-subtitle
-                  class="text--primary"
-                  v-text="item.headline"
-                ></v-list-item-subtitle>
+                <v-list-item-subtitle v-text="item.data"></v-list-item-subtitle>
 
-                <v-list-item-subtitle v-text="item.subtitle"></v-list-item-subtitle>
               </v-list-item-content>
-
-              <v-list-item-action>
-                <v-list-item-action-text v-text="item.action"></v-list-item-action-text>
-
-                <v-icon
-                  v-if="!active"
-                  color="grey lighten-1"
-                >
-                  mdi-star-outline
-                </v-icon>
-
-                <v-icon
-                  v-else
-                  color="yellow darken-3"
-                >
-                  mdi-star
-                </v-icon>
-              </v-list-item-action>
             </template>
           </v-list-item>
 
@@ -73,34 +50,40 @@
         {
           action: '15 min',
           headline: 'Brunch this weekend?',
-          subtitle: `I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
+          data: `I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
           title: 'Ali Connors',
         },
         {
           action: '2 hr',
           headline: 'Summer BBQ',
-          subtitle: `Wish I could come, but I'm out of town this weekend.`,
+          data: `Wish I could come, but I'm out of town this weekend.`,
           title: 'me, Scrott, Jennifer',
         },
         {
           action: '6 hr',
           headline: 'Oui oui',
-          subtitle: 'Do you have Paris recommendations? Have you ever been?',
+          data: 'Do you have Paris recommendations? Have you ever been?',
           title: 'Sandra Adams',
         },
         {
           action: '12 hr',
           headline: 'Birthday gift',
-          subtitle: 'Have any ideas about what we should get Heidi for her birthday?',
+          data: 'Have any ideas about what we should get Heidi for her birthday?',
           title: 'Trevor Hansen',
         },
         {
           action: '18hr',
           headline: 'Recipe to try',
-          subtitle: 'We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
+          data: 'We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
           title: 'Britta Holt',
         },
       ],
     }),
+
+    methods: {
+        goTo() {
+            window.location.href = "https://www.geeksforgeeks.org/";
+        }
+    }
   }
 </script>
