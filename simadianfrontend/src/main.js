8fec7d18a9a3434f4c7a6567d5e5f49980920662
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-05-29 20:25:24 +0530

    Added Google Map component

--- simadianfrontend/src/main.js
+++ simadianfrontend/src/main.js
@@ -5,7 +5,32 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 import VueSocialSharing from 'vue-social-sharing'
 import VueVideoPlayer from 'vue-video-player'
+import * as VueGoogleMaps from 'vue2-google-maps'
  
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyD-R2kjd7_hwso7GmVK3PGrGcu9gx1bJek',
+    libraries: 'places', // This is required if you use the Autocomplete plugin
+    // OR: libraries: 'places,drawing'
+    // OR: libraries: 'places,drawing,visualization'
+    // (as you require)
+ 
+    //// If you want to set the version, you can do so:
+    // v: '3.26',
+  },
+ 
+  //// If you intend to programmatically custom event listener code
+  //// (e.g. `this.$refs.gmap.$on('zoom_changed', someFunc)`)
+  //// instead of going through Vue templates (e.g. `<GmapMap @zoom_changed="someFunc">`)
+  //// you might need to turn this on.
+  // autobindAllEvents: false,
+ 
+  //// If you want to manually install components, e.g.
+  //// import {GmapMarker} from 'vue2-google-maps/src/components/marker'
+  //// Vue.component('GmapMarker', GmapMarker)
+  //// then disable the following:
+  // installComponents: true,
+})
 // require videojs style
 import 'video.js/dist/video-js.css'
 
