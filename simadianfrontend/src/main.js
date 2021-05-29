2b6b27fac6a468710f933b82e7cc3108abf4c5d1
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-05-29 09:21:05 +0530

    Added VideoPlayer

--- simadianfrontend/src/main.js
+++ simadianfrontend/src/main.js
@@ -4,11 +4,15 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import VueSocialSharing from 'vue-social-sharing'
+import VueVideoPlayer from 'vue-video-player'
+ 
+// require videojs style
+import 'video.js/dist/video-js.css'
 
 Vue.use(VueSocialSharing);
 
 Vue.config.productionTip = false
-
+Vue.use(VueVideoPlayer)
 
 new Vue({
   router,
