dc540a6d29a3929c217b48a6f9a7452a80dc20d4
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-06-04 15:35:26 +0530

    Added Logos - SocialSite

--- simadianfrontend/src/components/others/Test.vue
+++ simadianfrontend/src/components/others/Test.vue
@@ -1,118 +1,43 @@
 <template>
-<div
->   
     <v-card
-        max-width="150px"
+      style="display:flex;justify-content:space-evenly;max-width:700px;margin:auto;"
     >
-                <div
-                  style="padding:0px 5px;display:flex;justify-content:space-around;"
-                >
-                  <v-btn
-                    dark
-                    @click.stop="toggle ? endSpeechRecognition() : startSpeechRecognition()"
-                    icon
-                    :color="!toggle ? 'grey' : (speaking ? 'red' : 'red darken-3')"
-                    :class="{'animated infinite pulse': toggle}"
-                  >
-                    <v-icon>{{toggle ? 'mdi-microphone-off' : 'mdi-microphone'}}</v-icon>
-                  </v-btn>
-                  <v-btn
-                    dark
-                    @click="clearSentences"
-                    icon
-                    :color="!toggle ? 'grey' : (speaking ? 'red' : 'red darken-3')"
-                    :class="{'animated infinite pulse': toggle}"
-                  >
-                   <v-icon>mdi-delete</v-icon>
-                  </v-btn>
-                </div>
+    <ShareNetwork
+        v-for="(platform, index) in social_platforms"
+        :key="index"
+        :network="platform"
+        :url="url"
+        :title="title"
+        :description="description"
+        :quote="quote"
+        :hashtags="hashtags"
+    >
+        <img 
+            :src="require(`../../assets/logos/${platorm}.png`)" 
+            width="100px"
+        />
+    </ShareNetwork>
     </v-card>
-</div>
 </template>
 
 <script>
-
-let SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
-let recognition = SpeechRecognition? new SpeechRecognition() : false
-
 export default {
-  props: {
-    lang: {
-      type: String,
-      default: 'en-US'
-    }
-  },
   data () {
     return {
-      error: false,
-      speaking: false,
-      toggle: false,
-      runtimeTranscription: '',
-      sentences: [],
-      text: ""
+      url: "https://news.vuejs.org/issues/180",
+      title: "Say hi to Vite! A brand new, extremely fast development setup for Vue.",
+      description: "This week, I’d like to introduce you to 'Vite', which means 'Fast'. It’s a brand new development setup created by Evan You.",
+      quote: "The hot reload is so fast it\'s near instant. - Evan You",
+      hashtags: "vuejs,vite",
+
+      social_platforms: [
+        'whatsapp',
+        'facebook',
+        'redit',
+        'linkedin',
+        'twitter'
+      ]
     }
-  },
-  watch: {
-    'text' (val) {
-      this.$store.state.message = val;
-    }
-  },
-  methods: {
-    clearSentences () {
-      this.sentences = [];
-      this.text = "";
-    },
-    checkCompatibility () {
-      if (!recognition) {
-        this.error = 'Speech Recognition is not available on this browser. Please use Chrome or Firefox'
-      }
-    },
-    endSpeechRecognition () {
-      recognition.stop()
-      this.toggle = false;
-    },
-    startSpeechRecognition () {
-      if (!recognition) {
-        this.error = 'Speech Recognition is not available on this browser. Please use Chrome or Firefox'
-        return false
-      }
-      this.toggle = true
-      recognition.lang = this.lang
-      recognition.interimResults = true
-
-      recognition.addEventListener('speechstart', event => {
-        this.speaking = true
-      })
-
-      recognition.addEventListener('speechend', event => {
-        this.speaking = false
-      })
-
-      recognition.addEventListener('result', event => {
-        const text = Array.from(event.results).map(result => result[0]).map(result => result.transcript).join('')
-        this.runtimeTranscription = text
-      })
-
-      recognition.addEventListener('end', () => {
-        if (this.runtimeTranscription !== '') {
-          this.sentences.push(this.capitalizeFirstLetter(this.runtimeTranscription))
-          this.text = this.sentences.join('. ')
-        }
-        this.runtimeTranscription = ''
-        recognition.stop()
-        if (this.toggle) {
-          // keep it going.
-          recognition.start()
-        }
-      })
-      recognition.start()
-    },
-    capitalizeFirstLetter (string) {
-      return string.charAt(0).toUpperCase() + string.slice(1)
-    }
-  },
-  mounted () {
-    this.checkCompatibility()
   }
 }
 </script>
