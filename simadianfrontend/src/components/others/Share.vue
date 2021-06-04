06e3d9c337994c48014e44dc24a80561788379bd
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-06-04 16:01:21 +0530

    Added Share Custom Component

--- simadianfrontend/src/components/others/Share.vue
+++ simadianfrontend/src/components/others/Share.vue
@@ -1,15 +1,43 @@
 <template>
+    <v-card
+      style="display:flex;justify-content:space-evenly;margin:auto;padding-top:7px;"
+    >
     <ShareNetwork
-        network="whatsapp"
-        url="https://news.vuejs.org/issues/180"
-        title="Say hi to Vite! A brand new, extremely fast development setup for Vue."
-        description="This week, I’d like to introduce you to 'Vite', which means 'Fast'. It’s a brand new development setup created by Evan You."
-        quote="The hot reload is so fast it\'s near instant. - Evan You"
-        hashtags="vuejs,vite"
+        v-for="(platform, index) in social_platforms"
+        :key="index"
+        :network="platform"
+        :url="url"
+        :title="title"
+        :description="description"
+        :quote="quote"
+        :hashtags="hashtags"
     >
         <img 
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/WhatsApp.svg/1200px-WhatsApp.svg.png" 
-            width="50px"
+            :src="`https://github.com/jhabarsingh/SIMADIAN/blob/main/simadianfrontend/src/assets/logos/${platform}.png?raw=true`" 
+            width="100px"
         />
     </ShareNetwork>
+    </v-card>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      url: "https://news.vuejs.org/issues/180",
+      title: "Say hi to Vite! A brand new, extremely fast development setup for Vue.",
+      description: "This week, I’d like to introduce you to 'Vite', which means 'Fast'. It’s a brand new development setup created by Evan You.",
+      quote: "The hot reload is so fast it\'s near instant. - Evan You",
+      hashtags: "vuejs,vite",
+
+      social_platforms: [
+        'whatsapp',
+        'facebook',
+        'reddit',
+        'linkedin',
+        'twitter'
+      ]
+    }
+  }
+}
+</script>
--- simadianfrontend/src/components/others/Test.vue
+++ simadianfrontend/src/components/others/Test.vue
@@ -1,43 +1,46 @@
 <template>
-    <v-card
-      style="display:flex;justify-content:space-evenly;max-width:700px;margin:auto;"
-    >
-    <ShareNetwork
-        v-for="(platform, index) in social_platforms"
-        :key="index"
-        :network="platform"
-        :url="url"
-        :title="title"
-        :description="description"
-        :quote="quote"
-        :hashtags="hashtags"
-    >
-        <img 
-            :src="require(`../../assets/logos/${platorm}.png`)" 
-            width="100px"
-        />
-    </ShareNetwork>
-    </v-card>
+  <v-row justify="space-around">
+    <v-col cols="auto">
+      <v-dialog
+        transition="dialog-top-transition"
+        max-width="700px"
+        min-width="700px"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            color="grey lighten-3"
+            v-bind="attrs"
+            v-on="on"
+            fab
+            elevation="4"
+          >
+            <v-icon
+              large
+            >
+              mdi-share
+            </v-icon>
+          </v-btn>
+        </template>
+        <template v-slot:default="dialog">
+          <v-card
+            max-width="700px"
+            min-width="700px"
+          >
+              <Share />
+          </v-card>
+        </template>
+      </v-dialog>
+    </v-col>
+  </v-row>
 </template>
 
-<script>
-export default {
-  data () {
-    return {
-      url: "https://news.vuejs.org/issues/180",
-      title: "Say hi to Vite! A brand new, extremely fast development setup for Vue.",
-      description: "This week, I’d like to introduce you to 'Vite', which means 'Fast'. It’s a brand new development setup created by Evan You.",
-      quote: "The hot reload is so fast it\'s near instant. - Evan You",
-      hashtags: "vuejs,vite",
 
-      social_platforms: [
-        'whatsapp',
-        'facebook',
-        'redit',
-        'linkedin',
-        'twitter'
-      ]
-    }
+<script>
+import Share from './Share';
+
+export default {
+  components: {
+    Share,
   }
 }
 </script>
